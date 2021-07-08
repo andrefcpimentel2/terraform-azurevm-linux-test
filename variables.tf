4505f5d9b8160a986f1939d560431bcef4cabf3c
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2021-07-08 14:53:39 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "prefix" {
+  default = "azure-test"
+}
+
+variable "location" {
+  default = "UK West"
+}
+
+variable "username" {
+  default = "hashicorp"
+}
+
+variable "password" {
+  default = "changeMe123!"
+}
